844c775219d44f7857c0457fe01bed93f88b17f6
nanaGitH <lfebryano@gmail.com>
2024-02-04 20:38:26 +0700

    Products view

--- script.js
+++ script.js
@@ -38,3 +38,26 @@ function toggle(e) {
     this.closest('.has-child').classList.toggle('expand')
 }
 //gunanya closest()?
+
+//swiper/slider
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // // Navigation arrows
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+  
+    // // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
