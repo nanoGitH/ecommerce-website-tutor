dfac36a0cedefd4145dc882074260658e6c893ce
nanaGitH <lfebryano@gmail.com>
2024-02-05 18:02:03 +0700

    sekarang data product disimpan kedalam file json

--- script.js
+++ script.js
@@ -72,3 +72,90 @@ searchButton.addEventListener("click", function () {
 tClose.addEventListener("click", function () {
     showClass.classList.remove("showsearch");
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//with data in json
+const path = 'db.json'
+
+fetch(path).then(res => res.json()).then(res => {
+    const apparels = res.products.apparel
+    const electronics = res.products.electronics
+    const homes = res.products.home
+    const shoes = res.products.shoes
+    
+    let rowProducts1 = ""
+
+    apparels.forEach((el, i) => {
+        if(i === 4) return true
+        console.log(el.fileName)
+        rowProducts1 += `<div class="item">
+        <div class="media">
+          <div class="thumbnail object-cover">
+            <a href="#">
+              <img src="assets/products/${el.fileName}" alt="${el.productName}">
+            </a>
+          </div>
+          <div class="hoverable">
+            <ul>
+              <li class="active"><a href="#"><i class="ri-heart-line"></i></a></li>
+              <li><a href="#"><i class="ri-eye-line"></i></a></li>
+              <li><a href="#"><i class="ri-shuffle-line"></i></a></li>
+            </ul>
+          </div>
+          <div class="discount circle flexcenter"><span>32%</span></div>
+        </div>
+        <div class="content">
+          <h3 class="main-links"><a href="#">${el.productName}</a></h3>
+          <div class="rating">
+            <div class="stars"></div>
+            <span class="mini-text">${el.rating}</span>
+          </div>
+          <div class="price">
+            <span class="current">$129.99</span>
+            <span class="normal mini-text">${el.price}</span>
+          </div>
+          <div class="mini-text">
+            <p>${el.sold} sold</p>
+            <p>${el.freeShipping === true ? 'Free Shipiping' : ''}</p>
+          </div>
+        </div>
+      </div>`
+    })
+
+    const htmlPlace = document.querySelector('.row.products.mini')
+    htmlPlace.innerHTML = rowProducts1
+})
